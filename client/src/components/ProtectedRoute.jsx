2d61d031c39223e53902183aa5dc582698042e10
ductrinh5 <ngocduc181103@gmail.com>
2025-06-21 03:19:12 +0700

    new commit

--- client/src/components/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router";
-
-const ProtectedRoute = ({ children }) => {
-  const token = localStorage.getItem("token");
-  return token ? children : <Navigate to="/auth" replace />;
-};
-
-export default ProtectedRoute;
